d00d5b7e40801287148951b6605ca54795bc5258
deerr1 <furagkin678@gmail.com>
2022-04-09 04:13:45 +0500

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="box">
+    <div class="box2">
+      <h2>Введите текст для проверки:</h2>
+      <div v-if="result!=null">
+        <div v-if="result === true" class="positive">Позитивный текст</div>
+        <div v-else class="negative">Негативный текст</div>
+      </div>      <input class="inputRow" type="text" v-model="text"/>
+      <button @click="sendText">Проверить</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text: "",
+      result: null,
+    };
+  },
+  methods: {
+      sendText(){
+          axios
+            .post('http://localhost:8080/model', {"text": this.text})
+            .then(response => {
+                console.log(response.data)
+                this.result = response.data.result
+            }).catch(error => {
+                alert("Слова из текста не были найдены в модели")
+            })
+      }
   }
-}
+};
 </script>
+
+<style>
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 1000px;
+  width: 100%;
+}
+.box2 {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  height: 15%;
+  width: 500px;
+}
+.inputRow {
+  width: 50%;
+}
+button {
+  height: 30px;
+  width: 150px;
+}
+.positive {
+    background-color: rgb(180, 236, 95)
+}
+.negative {
+    background-color: rgb(255, 73, 73)
+}
+</style>
